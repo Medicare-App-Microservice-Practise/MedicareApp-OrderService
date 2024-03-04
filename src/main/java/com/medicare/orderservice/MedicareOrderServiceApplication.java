a5732813ee73edc3b87bf113bde8b308bfa4cdea
arvend <arvendrajan100@gmail.com>
2024-03-04 16:15:25 +0800

    initial commit

---
+++ src/main/java/com/medicare/orderservice/MedicareOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medicare.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicare/orderservice/MedicareOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicare.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
